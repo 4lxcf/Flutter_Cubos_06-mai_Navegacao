90644b666799c45ee03afcc2d70762558a55d2ff
Alexandre Ferreira <alexandrecferreira11@gmail.com>
2021-05-10 20:26:20 -0300

    Added FourthScreen

--- navigation/lib/screens/fourth_screen.dart
+++ navigation/lib/screens/fourth_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/routes.dart';
+
+class Result {
+  String name;
+  int age;
+}
 
 class FourthScreen extends StatelessWidget {
   const FourthScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Result item = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -15,13 +23,22 @@ class FourthScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).popUntil(
+                  ModalRoute.withName(Routes.HOME),
+                );
+              },
               child: Text('<- Go to Home Screen'),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).popAndPushNamed(Routes.THIRD);
+              },
               child: Text('<- Go back'),
             ),
+            item != null
+                ? Text('Name: ${item.name} // Age: ${item.age}')
+                : Text(''),
           ],
         ),
       ),
